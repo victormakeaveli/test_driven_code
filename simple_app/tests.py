748a518a7deeabee9247859f5dc604cc3291350f
Victor <victor.makeaveli@gmail.com>
2020-10-27 16:06:06 -0300

    writing more tests

--- simple_app/tests.py
+++ simple_app/tests.py
@@ -3,16 +3,33 @@ from django.test import Client
 from django.urls import reverse
 
 
-class testing_app(unittest.TestCase):
+class testing(unittest.TestCase):
 
-    def test_simple(self):    
+    def test_index(self):    
     
         #assume
         client = Client()
         response = client.get(reverse('index'))
-    
+
         #action
-        result = response.status_code
-    
+        response_code = response.status_code
+
         #assert
-        self.assertEquals(result, 200)
+        self.assertEquals(response_code, 200, "urls doesn't exist")
+
+        self.assertEquals(response.content, f'Hello, {user}', "message doesn't exists")
+
+    def test_user_input(self):
+
+        #assume
+        client = Client()
+        response = client.get(reverse('form'))
+
+        #action
+        response_code = response.status_code
+
+        #assert
+        #the page is ok
+        self.assertEquals(response_code, 200, "urls doesn't exist")
+        #the content is ok
+        # self.assertEqual(response.content) #todo
