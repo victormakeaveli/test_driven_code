5425092d352d162a4a99b89c2ffdc7f91163a35c
Victor <victor.makeaveli@gmail.com>
2020-10-28 16:47:14 -0300

    adding more tests

--- simple_app/tests.py
+++ simple_app/tests.py
@@ -1,59 +1,66 @@
 import unittest
+
 from django.test import Client
 from django.urls import reverse
 
 
 class testing(unittest.TestCase):
 
+    #assume
+    def setUp(self):
+        self.client = Client()
+        self.validator = Validator()
+        self.username = 'Normal'
+        self.user_age = 15
+    
+    def tearDown(self):
+        pass
+
     def test_index_and_greeting(self):    
     
-        #assume
-        client = Client()
-        response = client.get(reverse('index'))
-
-        #assert
+        response = self.client.get(reverse('index'))
+        
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.content, 'Hello')
 
     def test_form_and_validation(self):
 
-        #assume
-        client = Client()
-        validator = Validator()
+        self.response = self.client.get(reverse('form'))
+        response_code = self.response.status_code
 
-        response = client.get(reverse('form'))
-        
-        username = 'Normal'
-        user_age = 15
-
-        #action
-        response_code = response.status_code
-
-        name_validation = validator.username_is_valid(username)
-        age_validation = validator.user_age_is_valid(user_age)
+        #**validator will be a function inside the app**
+        name_validation = validator.username_is_valid(self.username)
+        age_validation = validator.user_age_is_valid(self.user_age)
         
         #assert
         #the url is ok
         self.assertEquals(response_code, 200)
 
         #it validates
-        self.assertTrue(name_validation)
-        self.assertTrue(age_validation)
-    
+        with self.assertTrue:
+            name_validation
+            age_validation
+
+        with self.assertRaises(ValueError):
+             name_validation, int 
+             age_validation, str 
+
+
     def test_bartender(self):
 
         #assume
-        user_age_1 <= 10
-        user_age_2 <= 20
-        user_age_3 <= 30
+        user_age_less < self.user_age
+        user_age_equal = self.user_age
+        user_age_more > self.user_age
          
         #action
-        what_drink1 = calculous.drinks(user_age_1)
-        what_drink2 = calculous.drinks(user_age_2)
-        what_drink3 = calculous.drinks(user_age_3)
+        #**calculous will be a function inside the app**
+        what_drink_less = calculous.drinks(user_age_less)
+        what_drink_equal = calculous.drinks(user_age_equal)
+        what_drink_more = calculous.drinks(user_age_more)
 
         #assert
-        self.assertEqual(what_drink1, 'milk')
-        self.assertEqual(what_drink2, 'coca-cola')
-        self.assertEqual(what_drink3, 'vodka')
+        self.assertEqual(what_drink_less, 'milk')
+        self.assertEqual(what_drink_equal, 'coca-cola')
+        self.assertEqual(what_drink_more, 'vodka')
 
--- simple_app/views.py
+++ simple_app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello")
+    greeting = 'Hello'
+    return HttpResponse(greeting)
 
     
