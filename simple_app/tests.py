7d7177c0a3ccf57cd1d336d93bedde01dd2e9b38
Victor <victor.makeaveli@gmail.com>
2020-10-30 16:09:23 -0300

    gabriel helped

--- simple_app/tests.py
+++ simple_app/tests.py
@@ -2,8 +2,12 @@ import unittest
 
 from django.test import Client
 from django.urls import reverse
-import json
-# from .models import Users
+#from .models import Users
+
+#gabriel help
+#import json
+# 
+
 class Testing(unittest.TestCase):
 
     #assume
@@ -21,44 +25,48 @@ class Testing(unittest.TestCase):
         response = self.client.get(reverse('index'))
         
         self.assertEquals(response.status_code, 200)
-        print(dir(response))
-        self.AssertEquals((json.loads(response.content)), "hello")
-        #self.assertEquals(response.content, 'Hello')
+
+        #gabriel help
+        # print(dir(response))
+        # self.AssertEquals(json.loads(response.content), "hello")
+        #
+        
+        self.assertEquals(response.content, 'Hello')
     
 
-    # def test_user_validation(self):
+    def test_user_validation(self):
 
-    #     response = self.client.get(reverse('form'))
-    #     response_code = self.response.status_code
+        response = self.client.get(reverse('form'))
+        response_code = self.response.status_code
 
-    #     #**validator will be a function inside the app**
-    #     name_validation = self.validator.user_is_valid(self.username)
+        #**validator will be a function inside the app**
+        name_validation = self.validator.user_is_valid(self.username)
         
-    #     #assert
-    #     #the url is ok
-    #     self.assertEquals(response_code, 200)
+        #assert
+        #the url is ok
+        self.assertEquals(response_code, 200)
 
-    #     #it validates 
-    #     with self.assertRaises(ValueError):
-    #         self.user.name, int
-    #         self.user.age, str
+        #it validates 
+        with self.assertRaises(ValueError):
+            self.user.name, int
+            self.user.age, str
 
 
-    # def test_bartender(self):
+    def test_bartender(self):
 
-    #     #assume
-    #     user_age_less = self.user.age
-    #     user_age_equal = self.user.age
-    #     user_age_more > self.user.age
+        #assume
+        user_age_less = self.user.age
+        user_age_equal = self.user.age
+        user_age_more > self.user.age
 
-    #     #action
-    #     #**calculous will be a function inside the app**
-    #     which_drink_less = self.calculous(user_age_less)
-    #     which_drink_equal = self.calculous(user_age_equal)
-    #     which_drink_more = self.calculous(user_age_more)
+        #action
+        #**calculous will be a function inside the app**
+        which_drink_less = self.calculous(user_age_less)
+        which_drink_equal = self.calculous(user_age_equal)
+        which_drink_more = self.calculous(user_age_more)
                 
-    #     #assert
-    #     self.assertEqual(which_drink_less, 'milk')
-    #     self.assertEqual(which_drink_equal, 'coca-cola')
-    #     self.assertEqual(which_drink_more, 'vodka')
+        #assert
+        self.assertEqual(which_drink_less, 'milk')
+        self.assertEqual(which_drink_equal, 'coca-cola')
+        self.assertEqual(which_drink_more, 'vodka')
 
